f6032eab56e4aee8ad1a66f06bda3afab2a79b09
Fernando Heredia <71789047+nanocarp01@users.noreply.github.com>
2021-09-20 08:00:18 -0300

    Delete APIproject directory

--- APIproject/APIp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApipConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'APIp'
--- APIproject/APIp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import APIpListViews, APIpDetailViews
-
-
-urlpatterns = [
-    path('project/', APIpListViews.as_view(), name='project-list'),
-    path('project/<int:pk>/', APIpDetailViews.as_view(), name='project-detail'),
-]
